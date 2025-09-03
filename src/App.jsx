eeb1b6348e4b00e3a0940e5136407c100ff06eaf
Karthic <karthics004@gmail.com>
2025-09-03 19:43:04 +0530

    Added time

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(timerId);
+  }, [])
 
   return (
    <div>
-    
+    {time.toLocaleTimeString()}
    </div>
   );
 }
 
-export default App
+export default App;
