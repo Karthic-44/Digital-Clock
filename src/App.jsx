877860b6db72b5097226100a7d38bc43d207ab85
Karthic <karthics004@gmail.com>
2025-09-04 18:37:20 +0530

    Formatted Time

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,17 @@ function App() {
     const timerId = setInterval(() => setTime(new Date()), 1000);
     return () => clearInterval(timerId);
   }, [])
+ 
+  const formattedTime = time.toLocaleTimeString('en-US',
+   { hour: '2-digit',
+    minute:'2-digit',
+    second: '2-digit'
+
+   });
 
   return (
    <div>
-    {time.toLocaleTimeString()}
+    {formattedTime}
    </div>
   );
 }
